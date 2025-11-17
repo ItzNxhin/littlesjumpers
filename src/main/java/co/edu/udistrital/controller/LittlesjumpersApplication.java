6010221fafafdff6cf8a04e10df2418a9929286f
ItzNxhin <nahinpenarandamejia@hotmail.com>
2025-11-16 19:44:54 -0500

    first commit

---
+++ src/main/java/co/edu/udistrital/controller/LittlesjumpersApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlesjumpersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittlesjumpersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udistrital/controller/LittlesjumpersApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittlesjumpersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
