320804f5531b48c0341ab20da695bfd752a11f0a
ItzNxhin <nahinpenarandamejia@hotmail.com>
2025-12-08 00:15:05 -0500

    Endpoint para obtener los estudaintes del profesor

--- src/main/java/co/edu/udistrital/controller/ProfesorController.java
+++ src/main/java/co/edu/udistrital/controller/ProfesorController.java
@@ -1,10 +1,17 @@
 package co.edu.udistrital.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.udistrital.dto.EstudianteResponse;
 import co.edu.udistrital.service.AcademicoProfesorService;
 
 @RestController
@@ -14,5 +21,20 @@ public class ProfesorController {
 
     @Autowired
     private AcademicoProfesorService academicoProfesorService;
-    
+
+    /**
+     * Obtiene estudiantes por grupo
+     * GET /api/profesor/estudiantes/{profesorId}
+     */
+    @GetMapping("/estudiantes/{profesorId}")
+    public ResponseEntity<List<EstudianteResponse>> listarPorGrupo(@PathVariable Integer profesorId) {
+        try {
+            List<EstudianteResponse> estudiantes = academicoProfesorService
+                    .obtenerEstudiantesDelGrupoProfesor(profesorId);
+            return ResponseEntity.ok(estudiantes);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
